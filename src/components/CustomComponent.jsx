9488e02613561527c281c3a4a6d24f8c5ea2ba21
Antonio F. Gasco <antoniofgasco@gmail.com>
2024-11-04 13:07:44 +0000

    Added functionality to CustomComponent

--- src/components/CustomComponent.jsx
+++ src/components/CustomComponent.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import { useState } from 'react';
 
 function CustomComponent({ id, className }) {
+  const [count, setCount] = useState(0);
+
   return (
     <div id={id} className={className}>
       <h1>Hi, I'm a custom component</h1>
       <h3>and I'm being rendered dinamically :-)</h3>
+      <div className="buttonsContainer">
+        <button onClick={() => setCount(count - 1)}>{`<`}</button>
+        <h3>{count}</h3>
+        <button onClick={() => setCount(count + 1)}>{`>`}</button>
+      </div>
     </div>
   );
 }
